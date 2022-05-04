7a4d74c190c30b25d3759161c99a14b38b1f8da7
Ahsanadam <adamebadul@gmail.com>
2022-05-04 20:39:50 +0200

    Initial commit

---
+++ src/main/java/example/micronaut/Conference.java
@@ -0,0 +1,19 @@
+package example.micronaut;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.ReflectiveAccess;
+
+@Introspected
+@ReflectiveAccess
+public class Conference {
+
+    private final String name;
+
+    public Conference(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
